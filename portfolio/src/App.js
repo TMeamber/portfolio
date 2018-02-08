68da04725286330e40199b20eb7d47d2b15e4a75
Travis Meamber <tmeamber623@gmail.com>
2018-02-08 15:35:17 -0800

    skills javascript added on

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
-
+import Skills from './Components/Skills/Skills';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-      < Navbar />
-      < Home />
-      <About />
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
       </div>
     );
   }
---
+++ portfolio/src/Components/Skills/Skills.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Skills.css';
+import CSS3 from '../../Assets/css.png';
+import ReactLogo from '../../Assets/react.png';
+import JavaS from '../../Assets/javascript.png';
+import HTML5 from '../../Assets/html.png';
+import Git from '../../Assets/git.png';
+import NodeLogo from '../../Assets/node.png';
+import Postgre from '../../Assets/postgre.png';
+
+
+export default function Skills() {
+    return (
+      <div className="Skills_Main" id="skills">
+          <div className="Skills_Header">
+              <h1>Skills</h1>
+              <hr/>
+          </div>
+          <div className="Skills_Container">
+              <div className="Front">
+                  <div className="Technologies">
+                  <p>HTML5</p>
+                  <p>JavaScript(ES6)</p>
+                  <p>ReactJS</p>
+                  <p>Redux</p>
+                  </div>
+              </div>
+              <div className="UI_UX">
+                  <div className="Technologies">
+                  <p>CSS3</p>
+                  <p>Material UI</p>
+                  <p>BootStrap</p>
+                  </div>
+              </div>
+              <div className="Back">
+                  <div className="Technologies">
+                  <p>Node.js</p>
+                  <p>Express</p>
+                  <p>PostgreSQL</p>
+                  </div>
+              </div>
+          </div>
+          <div className="Tech_Icon_Container">
+              <div className="Tech_Icons">
+                  <div className="Tech_icon"><img src={CSS3} alt="csslogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={HTML5} alt="htmllogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={JavaS} alt="jslogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={ReactLogo} alt="reactlogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={NodeLogo} alt="nodelogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={Postgre} alt="postgreslogo" className="Tech_Logo"/></div>
+                  <div className="Tech_icon"><img src={Git} alt="gitlogo" className="Tech_Logo"/></div>
+              </div>
+          </div>
+      </div>
+    );
+  }
+  
